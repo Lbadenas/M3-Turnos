3b2fb7d74e0c1e6df269f95997eaf5b66a8827d9
Luciano Badenas <163030721+Lbadenas@users.noreply.github.com>
2024-09-04 04:00:42 -0300

    marron

--- Front/src/views/home/home.jsx
+++ Front/src/views/home/home.jsx
@@ -6,14 +6,19 @@ function Home() {
     <div className={styles.homeContainer}>
       <div className={styles.overlay}></div>
       <div className={styles.textContainer}>
-        <h1 className={styles.welcomeText}>Bienvenido a nuestra peluquería</h1>
+        <h1 className={styles.welcomeText}>
+          ¡Bienvenido a la Experiencia de Belleza Definitiva!
+        </h1>
         <p className={styles.description}>
-          En nuestra peluquería te ofrecemos los mejores servicios para el
-          cuidado y estilo de tu cabello. ¡Déjanos transformar tu look!
+          En nuestro salón, no solo cuidamos tu cabello, sino que realzamos tu
+          estilo y personalidad. Desde cortes vanguardistas hasta tratamientos
+          exclusivos, estamos aquí para hacer que te sientas espectacular.
+          ¡Descubre la magia de una transformación que va más allá de lo
+          estético!
         </p>
       </div>
       <Link to="/appointmentsform">
-        <button className={styles.button}>reserva</button>
+        <button className={styles.button}>¡Reserva tu estilo!</button>
       </Link>
     </div>
   );
