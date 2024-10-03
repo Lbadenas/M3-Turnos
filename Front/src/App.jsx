d13b16090747c7a79ea65275d0be3e893038424b
Luciano Badenas <163030721+Lbadenas@users.noreply.github.com>
2024-10-03 13:00:29 -0300

    no footer

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -10,7 +10,6 @@ import AppointmentForm from "./components/appointmentsform/AppointmentsForm";
 import Contact from "./views/contact/Contact";
 import Servicios from "./views/services/Servicios";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import Footer from "./components/footer/Footer";
 
 import "./App.css";
 
@@ -35,8 +34,6 @@ function App() {
               <Route path="/appointmentsform" element={<AppointmentForm />} />
               <Route path="/*" element={<ErrorPage />} />
             </Routes>
-
-            <Footer />
           </div>
         </CSSTransition>
       </TransitionGroup>
